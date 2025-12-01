6ca1925c6eafd368a623eed4c8c05bb62ac881fc
danqzq <djumagaliyev@myseneca.ca>
2025-11-30 19:04:58 -0500

    Add XML documentation for DanNetEventAttribute and ExternalThreadEventAttribute

--- Assets/DanNet/Scripts/Runtime/Net/Attributes/DanNetEventAttribute.cs
+++ Assets/DanNet/Scripts/Runtime/Net/Attributes/DanNetEventAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Dan.Net
 {
+    /// <summary>
+    /// Indicates that the function/method attributed is a DanNet event.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class DanNetEventAttribute : Attribute { }
 }
--- Assets/DanNet/Scripts/Runtime/Net/Attributes/ExternalThreadEventAttribute.cs
+++ Assets/DanNet/Scripts/Runtime/Net/Attributes/ExternalThreadEventAttribute.cs
@@ -1,4 +1,7 @@
 ﻿namespace Dan.Net
 {
+    /// <summary>
+    /// Indicates that the function/method attributed is called on an external thread.
+    /// </summary>
     internal class ExternalThreadEventAttribute : System.Attribute { }
 }
